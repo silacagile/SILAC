4e7e8a27d05136d7f68d1726cc7a4e1fb1c5fa16
Jorge Aguirre <jagojr.a@gmail.com>
2014-12-04 19:51:10 -0400

    travis debe fallar

---
+++ test/Utilitarios/EmailValidatorTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Utilitarios;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jorge Aguirre
+ */
+public class EmailValidatorTest {
+    
+    public EmailValidatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of validate method, of class EmailValidator.
+     */
+    @Test
+    public void testValidateFalse() {
+        System.out.println("validate");
+        // Empty string should not passed the validation
+        String hex = "";
+        EmailValidator instance = new EmailValidator();
+        boolean expResult = true;
+        boolean result = instance.validate(hex);
+        assertEquals(expResult, result);
+    }
+    
+}
