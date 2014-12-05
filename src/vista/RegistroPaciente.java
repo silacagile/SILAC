ec7e728622194099b3c8b3aff5600863c446de31
Daniel Rocha <danieling.rocha@gmail.com>
2014-12-04 22:00:39 -0400

    elim

--- src/vista/RegistroPaciente.java
+++ src/vista/RegistroPaciente.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vista;
 
 import Utilitarios.EmailValidator;
@@ -14,7 +9,6 @@ import javax.swing.table.DefaultTableModel;
 import modelo.Paciente;
 
 /**
- *
  * @author Daniel
  */
 public class RegistroPaciente extends javax.swing.JFrame {
